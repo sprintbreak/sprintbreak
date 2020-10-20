df9428fb30a8b7a65b323022c18b2fd34f6cfa37
SPRINTBREAK <sprintbreak@coso-ad.com>
2020-10-20 03:49:18 -0300

    features: nerdearla 2020

--- js/ranking.js
+++ js/ranking.js
@@ -1,9 +1,21 @@
 const apiUrl = "https://sprintbreak.nerdear.live/ranking";
 
 function getRanking() {
+    var rankingData;
+    var rankingDom = document.querySelector('.premios-dos .ranking-names');
+    var ordList = document.createElement("ol");
     fetch(apiUrl)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+        rankingData = data.slice(0, 5); // Los primeros 6
+        rankingDom.remove();
+        rankingData.map(e => {
+            var li = document.createElement("li");
+            li.textContent = e.username;
+            ordList.appendChild(li);
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function () {
