22aa5700c5c77bc4becc44162fd3c3b43e6e533d
SPRINTBREAK <sprintbreak@coso-ad.com>
2020-10-20 15:35:13 -0300

    features: nerdearla 2020

--- js/ranking.js
+++ js/ranking.js
@@ -1,26 +1,22 @@
 const apiUrl = "https://sprintbreak.nerdear.live/ranking";
 
-function renderRanking(data) {
-    var rankingNames = document.querySelector('.premios-dos .ranking-names');
-    var rankingDomOl = document.querySelector('.premios-dos .ranking-names ol');
-    rankingDomOl.remove();
-    // var newRankingNames = document.createElement("div").className("ranking-names");
-    var newOrdList = document.createElement("ol");
-    var rankingData = data.slice(0, 5); // Los primeros 6
-    console.log(rankingData);
-    rankingData.map(e => {
-        var li = document.createElement("li");
-        li.textContent = e.username;
-        newOrdList.appendChild(li);
-    })
-    rankingNames.appendChild(newOrdList);
-}
-
 function getRanking() {
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        renderRanking(data);
+        var rankingNames = document.querySelector('.premios-dos .ranking-names');
+        var rankingDomOl = document.querySelector('.premios-dos .ranking-names ol');
+        rankingDomOl.remove();
+        // var newRankingNames = document.createElement("div").className("ranking-names");
+        var newOrdList = document.createElement("ol");
+        var rankingData = data.slice(0, 5); // Los primeros 6
+        console.log(rankingData);
+        rankingData.map(e => {
+            var li = document.createElement("li");
+            li.textContent = e.username;
+            newOrdList.appendChild(li);
+        })
+        rankingNames.appendChild(newOrdList);
     })
 }
 
